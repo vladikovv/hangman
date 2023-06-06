2724e8ba544a6aa96c3ba9e48bc3cb17a97760c1
Stoycho Vladikov <stoycho-vladikov@mail.bg>
2023-06-06 13:46:51 +0300

    Add game class

--- script.rb
+++ script.rb
@@ -1,11 +1,17 @@
 require 'open-uri'
 
-def generate_random_word
-  words = []
-  URI.open('words.txt').each do |word|
-    words.push(word.chomp) if word.length >= 5 && word.length <= 12
+class Game
+  def initialize
+    @word = generate_random_word
+    @max_incorrect_guesses = 10
+    @correct_guesses = 0
   end
-  words.sample
+
+  def generate_random_word
+    words = []
+    URI.open('words.txt').each do |word|
+      words.push(word.chomp) if word.length >= 5 && word.length <= 12
+    end
+    words.sample
+  end
 end
-
-

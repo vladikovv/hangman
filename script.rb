51eaba2e554e6c3efc00f227c1576dc801c83a1a
Stoycho Vladikov <stoycho-vladikov@mail.bg>
2023-06-16 12:42:28 +0300

    Add guess functionality

--- script.rb
+++ script.rb
@@ -1,10 +1,13 @@
 require 'open-uri'
 
 class Game
+  attr_accessor :word, :correct_guesses, :correct_letters
+
   def initialize
     @word = generate_random_word
     @max_incorrect_guesses = 10
     @correct_guesses = 0
+    @correct_letters = []
   end
 
   def generate_random_word
@@ -14,4 +17,15 @@ class Game
     end
     words.sample
   end
+
+  def guess(letter)
+    letter.downcase!
+    if word.include?(letter)
+      @correct_guesses += 1
+      correct_letters.push(letter)
+    end
+  end
 end
+
+game = Game.new
+game.guess("X")
